e24c7739769da7231030bb68ba32fee16d4158b2
Michael <1216866818@qq.com>
2026-03-02 12:31:32 +0800

    Create: 改回原来的接口

---
+++ include/corona/systems/ui/vulkan_backend.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <CabbageHardware.h>
+#include <SDL3/SDL.h>
+#include <imgui.h>
+
+#include <memory>
+
+namespace Corona::Systems 
+{
+    class VulkanBackend 
+    {
+    public:
+        VulkanBackend() = default;
+        ~VulkanBackend() = default;
+
+        bool initialize(SDL_Window* window);
+        void shutdown();
+
+        void new_frame();
+        void render_frame(ImDrawData* draw_data);
+        void present_frame();
+
+        [[nodiscard]] bool is_rebuild_needed() const { return rebuild_needed_; }
+        void request_rebuild() { rebuild_needed_ = true; }
+        void rebuild(int width, int height);
+    private:
+        SDL_Window* window_ = nullptr;
+        bool initialized_ = false;
+        bool rebuild_needed_ = false;
+
+        std::unique_ptr<HardwareDisplayer> displayer_;
+        HardwareExecutor executor_;
+    };
+
+}  // namespace Corona::Systems
